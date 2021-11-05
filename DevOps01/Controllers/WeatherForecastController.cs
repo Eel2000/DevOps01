dfb2ea3c7af5e10a1bf6f962bb5650fbd642e307
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-11-05 15:39:23 +0100

    remove newest thing

--- DevOps01/Controllers/WeatherForecastController.cs
+++ DevOps01/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DevOps01.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Unknown"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
